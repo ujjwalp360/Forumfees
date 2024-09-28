50c508185aa851770917fe2b93200f386e253714
ujjwalp360 <145079274+ujjwalp360@users.noreply.github.com>
2024-09-28 17:55:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -86,7 +86,6 @@ if st.button("Show List"):
         df_sorted = df.sort_values(by='Roll No').reset_index(drop=True)  # Reset index for clean display
         df_sorted.index += 1 #to show index 1
         st.write(df_sorted.astype(str))
-        st.write(df_sorted)
         
         # Calculate and display total amount
         total_amount = df['Amount'].sum()
